7e3873f067a46ad5ba3574d5a656799b50bdcbbf
segunemma2003 <segunemma2003@gmail.com>
2020-11-03 15:46:31 +0100

    check

--- app/Models/Post.php
+++ app/Models/Post.php
@@ -13,8 +13,8 @@ class Post extends Model
 
     protected $primaryKey = 'ID';
 
-    protected $fillable = ['post_author','post_date','post_date_gmt','post_content','post_title','post_excerpt','post_status','comment_status','ping_status','post_password','post_name','to_ping','pinged','post_modified','post_modified_gmt','post_content_filtered','post_parent','guid','menu_order','post_type','post_mime_type','comment_count'];
-
+    // protected $fillable = ['post_author','post_date','post_date_gmt','post_content','post_title','post_excerpt','post_status','comment_status','ping_status','post_password','post_name','to_ping','pinged','post_modified','post_modified_gmt','post_content_filtered','post_parent','guid','menu_order','post_type','post_mime_type','comment_count'];
+    protected $guarded=['ID'];
     public function author()
     {
         return $this->belongsTo('App\Models\User', 'post_author', 'ID');
